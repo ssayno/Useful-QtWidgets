ed20fe00d8adaba18b317a4c46e1297ae06bb71b
gl20227 <418215971@qq.com>
2022-10-31 13:53:23 +0800

    add progressbar example.

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLCDNumber, QLabel, QMainWindow, QApplication, QPushButton, QSplitter, QStackedWidget, QVBoxLayout, QWidget
+from src.progressbar import OwnProgressbar
+
+class MainUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(400, 400)
+        self.cw = QStackedWidget(self)
+        self.setCentralWidget(self.cw)
+        #
+        self.first_day = QWidget(self)
+        self.fd_layout = QVBoxLayout()
+        self.first_day.setLayout(self.fd_layout)
+        #
+        self.cw.addWidget(self.first_day)
+        #
+        self.add_widget()
+
+    def add_widget(self):
+        spl = QSplitter()
+        spl.setOrientation(Qt.Vertical)
+        button = QPushButton("Start Animation")
+        self.opb = OwnProgressbar(self)
+        self.opb.hasFocus()
+        spl.addWidget(button)
+        spl.addWidget(self.opb)
+        self.fd_layout.addWidget(spl)
+        button.clicked.connect(self.show_progressbar)
+
+    def show_progressbar(self):
+        self.opb.set_value()
+
+if __name__ == '__main__':
+    print("用于记录一些常用的 Qt 控件")
+    app = QApplication(sys.argv)
+    mu = MainUI()
+    mu.show()
+    sys.exit(app.exec_())
---
+++ src/progressbar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from PyQt5.QtCore import QPropertyAnimation, QTimer, Qt
+from PyQt5.QtWidgets import QProgressBar, QStyleFactory
+
+
+class OwnProgressbar(QProgressBar):
+    def __init__(self, parent) -> None:
+        super(OwnProgressbar, self).__init__(parent=parent)
+        # for Mac, you should add this to show percentage
+        self.setStyle(QStyleFactory.create('Fusion'))
+        self.setAlignment(Qt.AlignCenter)
+        self.setFormat('Loading... %p%')
+        self.setRange(0, 100)
+        # create animation
+        self.create_panimation()
+        # set a status
+        self.allow_next = True
+        self.setOrientation(Qt.Horizontal)
+        # self.setOrientation(Qt.Vertical)
+        self.valueChanged.connect(self.value_change)
+
+    def setValue(self, value: int) -> None:
+        self.old_value = self.value()
+        return super().setValue(value)
+
+    def set_value(self):
+        if self.allow_next:
+            self.allow_next = False
+            for i in range(1, 101):
+                QTimer.singleShot(20 * i, lambda x=i: self.setValue(x))
+
+    def value_change(self, _value):
+        #if self.isMaximized(): isMaximized not useful
+        if _value == self.maximum():
+            self.allow_next = True
+        self._animation.start()
+
+    def create_panimation(self):
+        if not hasattr(self, "old_value"):
+            self.old_value = self.value()
+        self._animation = QPropertyAnimation(self, b'value', self.parent())
+        self._animation.setStartValue(self.value())
+        self._animation.setEndValue(self.old_value)
